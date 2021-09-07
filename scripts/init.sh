eb91350b5a06527d629bd4dc3a039e99d78fda55
ITK13201 <tkik13201@gmail.com>
2021-09-07 22:19:42 +0900

    initial commit

---
+++ scripts/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+PROJECT_DIR=..
+
+cd `dirname $0`
+
+# init environment variables
+cp -v ${PROJECT_DIR}/.env.example ${PROJECT_DIR}/.env
+
+# init mysql log files
+mkdir -p ${PROJECT_DIR}/log/mysql
+touch ${PROJECT_DIR}/log/mysql/mysqld.log
+
+# init nextcloud data directory
+mkdir -p ${PROJECT_DIR}/docker/nextcloud/data
+
+# init log file permission
+find ${PROJECT_DIR}/log -type f -print | xargs chmod 666
